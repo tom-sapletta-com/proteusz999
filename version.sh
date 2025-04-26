15d1fb64910458fbcd7be43d7d13c9fc568ee42b
Tom Softreck <info@softreck.dev>
2025-04-26 22:08:18 +0200

    Release version 0.1.35

--- version.sh
+++ version.sh
@@ -2,6 +2,6 @@
 ./flatedit
 cp README.md Proteusz999.md
 #md2pdf Proteusz999.md Proteusz999.pdf
-md2pdf Proteusz999.md Proteusz999.pdf --css-file=md2pdf/style.css --mermaid-options='{"theme":"neutral","securityLevel":"loose"}'
+md2pdf README.md Proteusz999.pdf --css-file=md2pdf/style.css --mermaid-options='{"theme":"neutral","securityLevel":"loose"}'
 python changelog.py
 bash git.sh
